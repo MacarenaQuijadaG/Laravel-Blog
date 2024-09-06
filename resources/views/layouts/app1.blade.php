a3ebd0200e35951bc5826ad40b1aea49a4958304
MacarenaQuijadaG <mcarena.quijada@outlook.com>
2024-09-06 00:26:39 -0400

    Se actualiza los cambios

--- resources/views/layouts/app1.blade.php
+++ resources/views/layouts/app1.blade.php
@@ -3,11 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>My aplicacion @yield('title')</title>
 </head>
 <body>
     @section('name')
 este es el mensaje desde el loyaut
     @endsection
+
+
+    <div class ="container">
+        @yield('content')
+
+    </div>
 </body>
 </html>
