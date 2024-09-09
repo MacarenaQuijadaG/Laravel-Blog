8c631b78822f3de2fe34bdf547efd31b71920e9e
MacarenaQuijadaG <mcarena.quijada@outlook.com>
2024-09-09 00:36:31 -0300

    Se añaden cambios en los componentes mostrandolos en la vista como mensajes de alertas

--- app/View/Components/Alert.php
+++ app/View/Components/Alert.php
@@ -4,16 +4,23 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class Alert extends Component
+class Alerta extends Component
 {
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct()
+
+    public $type;
+    public $mensaje;
+    public function __construct($type , $mensaje)
     {
-        //
+    
+    $this->type=$type;
+    $this->mensaje=$mensaje;
+
+
     }
 
     /**
@@ -25,4 +32,7 @@ class Alert extends Component
     {
         return view('components.alerta');
     }
+
+    
+    
 }
--- resources/views/components/alerta.blade.php
+++ resources/views/components/alerta.blade.php
@@ -1,3 +1,6 @@
 <div>
     <h2>_Este es mi nuevo componente</h2>
+    <h1>El error de tipo es: {{$type}}</h1>
+    <h4>este es el mensaje: {{$mensaje}}</h4>
+
 </div>
